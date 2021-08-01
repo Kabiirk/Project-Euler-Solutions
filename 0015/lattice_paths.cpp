fcb36367ebc38f33d217efa9a0b0e0e67610b050
Kabiir <kabiirk@gmail.com>
2021-08-01 23:37:58 +0530

    Sol. 15

--- 0015/lattice_paths.cpp
+++ 0015/lattice_paths.cpp
@@ -6,12 +6,12 @@ Starting in the top left corner of a 2×2 grid, and only being able to move to th
 
 How many such routes are there through a 20×20 grid?
 
-Answer : 
+Answer : 137846528820
 */
 
 /*
 TIPS:
-
+This can be solved using combinatronics
 */
 
 #include <bits/stdc++.h>
@@ -19,6 +19,14 @@ using namespace std;
 
 int main()
 {
+	int gridSize = 20;
+	long long int paths = 1;
+ 
+	for (int i = 0; i < gridSize; i++) {
+		paths *= ((2 * gridSize) - i);
+    	paths /= (i + 1);
+	}
+	cout<<paths<<endl;
     
     return 0;
 }

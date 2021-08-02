a6c402ed71a9b5e15843356dd55b9c4bf6b894b7
Kabiir <kabiirk@gmail.com>
2021-08-02 23:29:40 +0530

    Sol. 16 Done

--- 0016/power_digit_sum.cpp
+++ 0016/power_digit_sum.cpp
@@ -4,7 +4,7 @@ Problem 16
 
 What is the sum of the digits of the number 2^1000?
 
-Answer : 
+Answer : 1366
 */
 
 /*
@@ -14,32 +14,52 @@ TIPS:
 
 
 #include <iostream>
-#include <vector>
+#include <cstring>
 #include <bits/stdc++.h>
 
 using namespace std;
-
-int digitSum(int n){
-	int sum = 0;
-	while (n != 0){
-		sum = sum + n % 10;
-        n = n / 10;
+ 
+const int MAXN = 1000;
+ 
+int result[MAXN+1];
+ 
+int power(int a, int n)
+{
+    int digits, j;
+ 
+    memset(result, 0, sizeof(result));
+ 
+    result[0] = a;
+    digits = 1;
+    for(int i=2; i<=n; i++) {
+        int carry = 0;
+ 
+        for(j=0; j<digits; j++)
+            result[j] *= a;
+ 
+        for(j=0; j<=digits; j++) {
+            result[j] += carry;
+            carry = result[j] / 10;
+            result[j] %= 10;
+        }
+        digits = j;
     }
-    
-	return sum;
+ 
+    return digits;
 }
-
+ 
 int main()
 {
-	vector<unsigned int> digits;
-	digits.push_back(1);
-	digits.push_back(2);
-	digits.push_back(3);
-	
-	for (int i = 0; i < digits.size(); i++) {
-            cout << i<<" : "<<digits[i] << "\n";
-        }
-	
+    int n=1000;
+	int digits;
+
+    digits = power(2, n);
+    int sum = 0;
+    for(int i=0; i<=digits; i++)
+       sum += result[i];
     
+	cout << sum << endl;
+
     return 0;
 }
+

12f028731157839ceabdef6c0ab0ee24d32c3735
Kabiir <kabiirk@gmail.com>
2021-08-08 02:53:01 +0530

    Sol. 20 (rough iteration)

---
+++ 0020/factorial_digit_sum.cpp
@@ -0,0 +1,87 @@
+/*
+Problem 20
+n! means n × (n - 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+
+Answer : 
+*/
+
+/*
+TIPS:
+*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define MAX 500
+ 
+int multiply(int x, int res[], int res_size);
+ 
+// This function finds factorial of large numbers
+// and prints them
+int factorialSum(int n)
+{
+    int res[MAX];
+ 
+    // Initialize result
+    res[0] = 1;
+    int res_size = 1;
+ 
+    // Apply simple factorial formula n! = 1 * 2 * 3 * 4...*n
+    for (int x=2; x<=n; x++)
+        res_size = multiply(x, res, res_size);
+
+    int sum = 0;
+    for (int i=res_size-1; i>=0; i--)
+    	sum+=res[i];
+        //cout << res[i];
+    return sum;
+}
+ 
+// This function multiplies x with the number
+// represented by res[].
+// res_size is size of res[] or number of digits in the
+// number represented by res[]. This function uses simple
+// school mathematics for multiplication.
+// This function may value of res_size and returns the
+// new value of res_size
+int multiply(int x, int res[], int res_size)
+{
+    int carry = 0;  // Initialize carry
+ 
+    // One by one multiply n with individual digits of res[]
+    for (int i=0; i<res_size; i++)
+    {
+        int prod = res[i] * x + carry;
+ 
+        // Store last digit of 'prod' in res[] 
+        res[i] = prod % 10; 
+ 
+        // Put rest in carry
+        carry  = prod/10;   
+    }
+ 
+    // Put carry in res and increase result size
+    while (carry)
+    {
+        res[res_size] = carry%10;
+        carry = carry/10;
+        res_size++;
+    }
+    return res_size;
+}
+
+int main()
+{
+	
+	cout<<factorialSum(100)<<endl;
+    
+    return 0;
+}

b50805420ccf6100f84c89f8e79dcad141587e36
Kabiir <kabiirk@gmail.com>
2021-08-22 23:44:29 +0530

    Redo Sol. 32

--- 0032/pandigital_products.cpp
+++ 0032/pandigital_products.cpp
@@ -28,59 +28,5 @@ TIPS:
 
 int main()
 {
-  unsigned int maxDigit=9;
 
-  std::vector<unsigned int> digits = { 1,2,3,4,5,6,7,8,9 };
-  // remove higher numbers so there is only 1..n left
-  digits.resize(maxDigit);
-
-  // all pandigital products
-  std::set<unsigned int> valid;
-
-  // create all permutations
-  do
-  {
-    for (unsigned int lenA = 1; lenA < maxDigit; lenA++)
-      for (unsigned int lenB = 1; lenB < maxDigit - lenA; lenB++)
-      {
-        unsigned int lenC = maxDigit - lenA - lenB;
-
-        if (lenC < lenA || lenC < lenB)
-          break;
-
-        unsigned int pos = 0;
-
-        unsigned int a = 0;
-        for (unsigned int i = 1; i <= lenA; i++)
-        {
-          a *= 10;
-          a += digits[pos++];
-        }
-
-        unsigned int b = 0;
-        for (unsigned int i = 1; i <= lenB; i++)
-        {
-          b *= 10;
-          b += digits[pos++];
-        }
-
-        unsigned int c = 0;
-        for (unsigned int i = 1; i <= lenC; i++)
-        {
-          c *= 10;
-          c += digits[pos++];
-        }
-
-        if (a*b == c)
-          valid.insert(c);
-      }
-  } while (std::next_permutation(digits.begin(), digits.end()));
-
-  // find sum
-  unsigned int sum = 0;
-  for (auto x : valid)
-    sum += x;
-  std::cout << sum << std::endl;
-
-  return 0;
 }

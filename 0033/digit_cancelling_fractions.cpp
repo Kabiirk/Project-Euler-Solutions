ed02e6c267892a0ecce1f3a6743bed269f57846e
Kabiir <kabiirk@gmail.com>
2021-08-25 00:50:43 +0530

    Sol. 33

--- 0033/digit_cancelling_fractions.cpp
+++ 0033/digit_cancelling_fractions.cpp
@@ -15,7 +15,7 @@ in the numerator and denominator.
 If the product of these four fractions is given in its lowest
 common terms, find the value of the denominator.
 
-Answer : 6857
+Answer : 
 */
 
 /*
@@ -25,42 +25,68 @@ i.e. (10->99)/(10->99)
 
 fraction should be lesser than 1
 i.e. numerator < denominator
+
+1) Brute Force approach
+   Try all possible fraction combinations with 2 digit numbers
+   as numerator and denominator, based on conditions find if
+   they are digit cancelling or not, then multiply the valid fractions.
+
+   Answer is the reduced denominator therefore we need to find gcd
+   of numerator and denominator and divide that by denominator to
+   get our answer. 
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+// Function to return gcd of a and b
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+
 // only for non-trivial solutions
 bool digitCancellingFractions(int num, int denom){
-    if(num%10==0 && denom%10==0){
-        return false; //trivial solution
-    }
     if(num > denom){
         return false; //fraction is more than 1
+    }
+    if(num==denom){
+        return false;
+    }
+    if(num%10==0 && denom%10==0){
+        return false; //trivial solution
     }
 
     int num_ones = num%10;
     int num_tens = num/10;
     int denom_ones = denom%10;
     int denom_tens = denom/10;
-    cout<<num_tens<<"-"<<num_ones<<"/"<<denom_tens<<"-"<<denom_ones<<endl;
     
     double actual_ans = (double)num/(double)denom;
+    double digit_cancel_ans;
 
-    bool num_ones_cancel = (num_ones==denom_ones)||(num_ones==denom_tens);
-    bool num_tens_cancel = (num_tens==denom_ones)||(num_tens==denom_tens);
-    cout<<num_ones_cancel<<endl;
-    cout<<num_tens_cancel<<endl;
-    cout<< (int)num_ones_cancel*num_ones + (int)num_tens_cancel*num_tens <<endl;
-    cout<< (int)num_ones_cancel*denom_ones + (int)num_tens_cancel*denom_tens <<endl;
-
-    double digit_cancel_ans = (double)((int)num_ones_cancel*num_ones + (int)num_tens_cancel*num_tens)/(double)((int)num_ones_cancel*denom_ones + (int)num_tens_cancel*denom_tens) ;
-    cout<<digit_cancel_ans<<endl;
-    cout<<actual_ans<<endl;
+    // can be done better
+    if(num_tens == denom_tens){
+        digit_cancel_ans = (double)num_ones/(double)denom_ones;
+    }
+    else if(num_tens == denom_ones){
+        digit_cancel_ans = (double)num_ones/(double)denom_tens;
+    }
+    else if(num_ones == denom_tens){
+        digit_cancel_ans = (double)num_tens/(double)denom_ones;
+    }
+    else if(num_ones == denom_ones){
+        digit_cancel_ans = (double)num_tens/(double)denom_tens;
+    }
+    else{
+        return false;
+    }
 
     if(actual_ans == digit_cancel_ans){
-        cout<<num<<"hjk"<<denom<<endl;
         return true;
     }
     return false;
@@ -68,8 +94,23 @@ bool digitCancellingFractions(int num, int denom){
 
 int main()
 {
-    int a = (int)digitCancellingFractions(49,98);
-    //cout<<a<<endl;
+    int init_num = 1;
+    int init_denom = 1;
+
+    // Iterate through solution space
+    for(int i=10;i<=99;i++){
+        for(int j=10;j<=99;j++){
+            if(digitCancellingFractions(i,j)){
+                init_num*=i;
+                init_denom*=j;
+                //cout<<i<<"/"<<j<<endl;
+            }
+        }
+    }
+
+    int reduced_denom = init_denom/gcd(init_num, init_denom);
+    
+    cout<<reduced_denom<<endl;
     
     return 0;
 }

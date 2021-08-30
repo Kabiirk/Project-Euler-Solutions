6d5d3e27bb0ea2cc3abb8d606f38704c0d99b7ba
Kabiir <kabiirk@gmail.com>
2021-08-30 23:25:47 +0530

    Main Code written

--- 0037/truncatable_primes.cpp
+++ 0037/truncatable_primes.cpp
@@ -20,13 +20,75 @@ TIPS:
 */
 
 #include <iostream>
-#include<string>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+vector<bool> sieveEro(int n){
+    vector<bool> is_prime(n+1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (is_prime[i] && (long long)i * i <= n) {
+            for (int j = i * i; j <= n; j += i){
+                is_prime[j] = false;
+            }
+        }
+    }
+
+    return is_prime;
+}
+
+bool allRightTruncatePrimes(int n, vector<bool> prime_list){
+    bool final = true;
+    while(n>0 && final){
+        n = n/10;
+        final = final && prime_list[n];
+    }
+    return final;
+}
+
+// helper function for allLeftTruncatePrimes
+int largestPowerTen(int n){
+    int factor = 1;
+    while(factor<n){
+        factor*=10;
+    }
+    return factor;
+}
+
+bool allLeftTruncatePrimes(int n, vector<bool> prime_list){
+    bool final = true;
+    int factor = largestPowerTen(n);
+    while(n>0 && final){
+        n = n%factor;
+        factor = factor/10;
+    }
+    return final;
+}
+
 int main()
 {
-    cout<<"Hello Euler !"<<endl;
+    vector<bool> a = sieveEro(100);
+    int limit = 1000;
+    int truncatable_prime_count=0;
+    int truncatable_prime_sum=0;
+    int i = 10;
+
+    while(truncatable_prime_count!=11){
+        if(a[i]){
+            if(allLeftTruncatePrimes(i, a) && allRightTruncatePrimes(i, a)){
+                truncatable_prime_sum+=i;
+                truncatable_prime_count+=1;
+            }
+        }
+        i++;
+    }
+
+
+    // for(int i = 0; i<a.size(); i++){
+    //     if(a[i]){
+    //         cout<<i<<endl;
+    //     }
+    // }
     return 0;
 }

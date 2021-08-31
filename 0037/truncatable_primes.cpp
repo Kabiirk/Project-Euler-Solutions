ae6a492fa7e88c6bbce2b75b15b4ad15f35532ec
Kabiir <kabiirk@gmail.com>
2021-08-31 15:26:33 +0530

    Sol. 37

--- 0037/truncatable_primes.cpp
+++ 0037/truncatable_primes.cpp
@@ -11,12 +11,34 @@ from left to right and right to left.
 
 NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 
-Answer : 
+Answer : 748317
 */
 
 /*
 TIPS:
+Create a sieve of primes uptill 1000000 (not bect practice due to stack issues)
+for each prime in that list check for left & right truncated primes then add accordingly
 
+Right truncation : 
+num/10 -> removes last digit
+e.g. :
+3797/10 = 379 (C++ outputs floor int value)
+379/10 = 37
+37/10 = 3
+
+Left truncation :
+a little tricker than right truncation
+we first need to find highest power of 10 grater than the number
+factor can be divided by 10 to reduce an extra step of checking original number for being a prime
+factor%num -> removes left-most digit
+factor/10 to scale down the factor since our left most digit is reduced, otherwise we will get the same answer on repeat
+e.g. :
+highest power of 10 greater than 3797 = 10000
+3797%10000 = 3797 (can be avoided by dividing factor by 10 first)
+3797%1000 = 797
+797%100 = 97
+97%10 = 7
+(also notice how factor is divided by 10 in each step so that we arent stuck on an answers)
 */
 
 #include <iostream>
@@ -71,29 +93,23 @@ bool allLeftTruncatePrimes(int n, vector<bool> prime_list){
 
 int main()
 {
-    vector<bool> a = sieveEro(100000);
-    int limit = 1000;
+    vector<bool> a = sieveEro(1000000); // find a better way to do this, usuallynot a good practice to create a vector having a million values
     int truncatable_prime_count=0;
     int truncatable_prime_sum=0;
     int i = 10;
-    int num = 3797;
-
-
-    // if(1){
-    //     cout<<allRightTruncatePrimes(num,a)<< endl;
-    //     cout<< allLeftTruncatePrimes(num,a) <<endl;
-    // }
-
 
     while(11 > truncatable_prime_count){
         if(a[i]){
             if(allRightTruncatePrimes(i,a) && allLeftTruncatePrimes(i,a)){
-                cout<<i<<endl;
+                //cout<<i<<endl;
                 truncatable_prime_count += 1;
+                truncatable_prime_sum += i;
             }
         }
         i = i+1;
     }
+
+    cout<<truncatable_prime_sum<<endl;
 
     return 0;
 }

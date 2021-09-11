ce21140c021dc330d1613f7afaa9143a334218df
Kabiir <kabiirk@gmail.com>
2021-09-12 02:43:52 +0530

    Modified script to accept exactly 1 Argument, else exit

--- automate_push.sh
+++ automate_push.sh
@@ -1,6 +1,12 @@
 # Run the srcipt with commit message as command line argument :
 # bash automate_push.sh "Some commit message"
 
+if [ $# -ne 1 ]
+  then
+    echo "Improper number of arguments supplied"
+    exit 1
+fi
+
 git add .
 git commit -m "$1"
 git push

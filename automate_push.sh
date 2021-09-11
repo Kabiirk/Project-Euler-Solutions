c1ca70977c47090d8376c9276fde414e23cd56a4
Kabiir <kabiirk@gmail.com>
2021-09-12 02:37:16 +0530

    'Script for sutomating push complete'

--- automate_push.sh
+++ automate_push.sh
@@ -1,1 +1,7 @@
-echo "'$1'"
+# Run the srcipt with commit message as command line argument :
+# bash automate_push.sh "Some commit message"
+
+git add .
+git commit -m "'$1'"
+git push
+echo "Changes Pushed Successfully !!"

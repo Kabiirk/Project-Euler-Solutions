06d37dc71f00ac9c79a9a7d4444bfdbae2f03807
Kabiir <kabiirk@gmail.com>
2021-09-16 00:07:59 +0530

    Sol. 50 new approach

--- 0050/consecutive_prime_sum.cpp
+++ 0050/consecutive_prime_sum.cpp
@@ -17,7 +17,29 @@ Answer : 997651 (543 terms)
 
 /*
 TIPS:
+Brute Forse iteration by using something similar to sliding window
 
+CAUTION : 
+(This caused me some headache apart from figuring out how to iterate)
+The problem asks for sums of CONSECUTIVE primes, which need not necessarily
+begin with the first prime. The fact that the shown example :
+
+41 = 2 + 3 + 5 + 7 + 11 + 13 
+
+starts with the first prime is incidental
+(although it is no accident that the winning sequence starts with at a small prime).
+
+953 =  7 + 11 + 13 + 17 + 19 + 23 + 29 
+    + 31 + 37 + 41 + 43 + 47 + 53 + 59 
+    + 61 + 67 + 71 + 73 + 79 + 83 + 89
+
+That's 21 terms. There is no mistake in the problem description - the term 'FIRST' does not appear anywhere in the text.
+Ref. : https://stackoverflow.com/questions/36854818/project-euler-50-prime-sums-not-correct
+
+Some other approaches:
+https://www.mathblog.dk/project-euler-50-sum-consecutive-primes/
+https://radiusofcircle.blogspot.com/2016/06/problem-50-project-euler-solution-with-python.html
+https://euler.stephan-brumme.com/50/
 */
 
 #include <iostream>
@@ -49,51 +71,49 @@ vector<bool> sieveOfEratosthenes(int n)
     return prime;
 }
 
-bool isPrime(int n)
-{
-    // Corner case
-    if (n <= 1)
-        return false;
-  
-    // Check from 2 to n-1
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
-  
-    return true;
-}
-
-vector<int> slidingWindow(int lmt, vector<bool> prime_bool){
-  vector<int> sum_window;
-  int i = 0;
-  int sum;
-  int i_offset = 0;
-  bool keep_going = true;
-
-  while(keep_going){
-    sum = 0;
-    while(sum<lmt){
-      i = i_offset;
-      sum+=prime_num[i];
-      i++;
-    }
-    cout<<sum<<" "<<i<<endl;
-    i_offset++;
-  }
-
-  return sum_window;
-}
-
 
 int main(){
-  int limit = 1000;
+  int limit = 1000000;
   vector<bool> prime_bool = sieveOfEratosthenes(limit);
-  int n = prime_num.size();
-  vector<int> sum_window = slidingWindow(100, prime_bool);
 
-  // for(int i = 0; i<sum_window.size();i++){
-  //   cout<<sum_window[i]<<endl;
-  // }
-  
-  
+  int sum, num_terms = 1;
+  for ( int i = 0; i + num_terms <= prime_num.size(); i++ ) {
+    int j = i + 2;
+    int s = prime_num[i] + prime_num[j - 1] + prime_num[j];
+
+    if (i == 0){
+      s -= prime_num[j--];
+    }
+
+    while ( s < 1000000 && j < prime_num.size() ) {
+      int l = j - i + 1;
+      if (l > num_terms && prime_bool[s]) {
+        num_terms = l;
+        sum = s;
+      }
+      j += 2;
+      s += prime_num[ j - 1 ] + prime_num[ j ];
+    }
+  }
+  cout << sum << endl;//" " << num_terms << endl;
+
+  return 0;
 }
+
+
+/*
+
+ __   __          
+ \ \ / /_ _ _   _ 
+  \ V / _` | | | |
+   | | (_| | |_| |
+   |_|\__,_|\__, |
+            |___/ 
+
+  _____ _          _     ____   ___    ____        _               _   _   _ 
+ |  ___(_)_ __ ___| |_  | ___| / _ \  / ___|  ___ | |_   _____  __| | | | | |
+ | |_  | | '__/ __| __| |___ \| | | | \___ \ / _ \| \ \ / / _ \/ _` | | | | |
+ |  _| | | |  \__ \ |_   ___) | |_| |  ___) | (_) | |\ V /  __/ (_| | |_| |_|
+ |_|   |_|_|  |___/\__| |____/ \___/  |____/ \___/|_| \_/ \___|\__,_| (_) (_)
+
+*/

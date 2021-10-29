c2a888422059185e2b62f864c720574c27c71c7b
Kabiir <kabiirk@gmail.com>
2021-10-30 01:12:32 +0530

    Added execution instructions and cleaned up code

--- run.sh
+++ run.sh
@@ -1,23 +1,36 @@
+# Bash script to run code irrespective of code being
+# written in C++ or Python. Precedence given to C++
+# if both files are present. It exits after executing
+# 1 either of the Codes.
+
+# All that needs to be done is type :
+# bash run.sh XXXX
+
+# where XXXX is diretory where .py or .exe(c++ executable)
+# is stored
+
+# e.g. : bash run.sh 0069
+# would look for file.exe and execute it if present
+# else it would look for file.py and execute that.
+
 if [ $# -ne 1 ]
   then
     echo "Improper number of arguments supplied"
     exit 1
 fi
 
+cd $1
+CPP_FILE=$(find . -type f -name "*.exe")
+
 if [ ${CPP_FILE} ]
   then
-    echo "File exists"
-    echo $PWD
-    CPP_FILE=$(find . -type f -name "*.exe")
     FILE_NAME="${CPP_FILE%.*}"
     ${FILE_NAME}
     cd ..
     exit 1
   else
-    echo $PWD
     PY_FILE=$(find . -type f -name "*.py" | sed "s|^\./||")
     FILE_NAME="${PY_FILE%.*}.py"
-    echo ${FILE_NAME}
     python ${FILE_NAME}
     cd ..
     exit 1

b27372ef7f120fa28afbce490fa6d21ae40781ea
Kabiir <kabiirk@gmail.com>
2022-08-23 16:56:15 +0530

    Corrected example command in automate_boilerplate.sh + Added problem statement for Sol. 67

--- automate_boilerplate.sh
+++ automate_boilerplate.sh
@@ -1,13 +1,13 @@
 # Makes new file_name.cpp  or file_name.py with given arguments
 # Run the srcipt with commit message as command line argument :
-# bash automate_copy_rename.sh XXXX file_name c++/py
+# bash automate_boilerplate.sh XXXX file_name c++/py
 #
 # Put 3rd argument either as 'c++' if working with file.cpp
 # or put it as py if you want to work with a file.py script
 # XXXX - Problem Number with leading zeros if it is less
 # than 4 digits
 # e.g. : If problem number is 69, command used would be 
-# bash automate_copy_rename.sh 0069 file_name
+# bash automate_boilerplate.sh 0069 file_name c++/py
 
 if [ $# -ne 3 ]
   then

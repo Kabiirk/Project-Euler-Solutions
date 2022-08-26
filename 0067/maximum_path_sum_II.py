5af5c0134192e2620936e83bbde104c5b1d7ba90
Kabiir <kabiirk@gmail.com>
2022-08-27 03:58:12 +0530

    Sol. 67 WIP

--- 0067/maximum_path_sum_II.py
+++ 0067/maximum_path_sum_II.py
@@ -32,12 +32,12 @@ num_triangle = []
 '''
 num_triangles = 
 [
-   ['59'],
-   ['73', '41'],
-   ['52', '40', '09'],
-   ['26', '53', '06', '34'],
-   ['10', '51', '87', '86', '81'],
-   ['61', '95', '66', '57', '25', '68'],
+   [59,  0,  0,  0,  0.  0],
+   [73, 41,  0,  0,  0,  0],
+   [52, 40,  9,  0,  0,  0],
+   [26, 53,  6, 34,  0,  0],
+   [10, 51, 87, 86, 81,  0],
+   [61, 95, 66, 57, 25, 68],
    ......
 ]
 
@@ -53,4 +53,24 @@ with open('p067_triangle.txt') as f:
       else:
          num_triangle.append(num_array)
 
-print(num_triangle)
+# Pad array with 0's at the end
+# Dynamically get length of last array
+# which is the largest array
+n = len(num_triangle[len(num_triangle)-1])
+for array in num_triangle:
+   for i in range(len(array),100):
+      array.append(0)
+
+# print(len(num_triangle[99]))
+def maxPathSum(triangle):
+   # bottom up addition
+   m = len(triangle)
+   n = len(triangle[0])
+   for i in range(m-2,0, -1):
+      print(i)
+      for j in range(0,i+1):
+         triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1])
+                          #     Left Child        Right Child
+   return triangle[0][0]
+
+print(maxPathSum(num_triangle))

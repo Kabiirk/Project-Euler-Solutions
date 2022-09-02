23ce45a1fd9c6bae2f00b7a0198fc95d3b268365
Kabiir <kabiirk@gmail.com>
2022-09-02 20:50:10 +0530

    Sol. 68

--- 0068/magic_5_gon_ring.py
+++ 0068/magic_5_gon_ring.py
@@ -63,92 +63,149 @@ What is the maximum 16-digit string for a "magic" 5-gon ring?
                    │   │
                    └───┘
 
-Answer :
+Answer : 6531031914842725
 '''
 
 '''
 TIPS:
+Iterate through differnt combinations of numbers to find
 
+For ref.:
+         ┌───┐
+         │ a │
+         └───┤
+             └─┐
+               ├───┐
+               │ b │
+               ├───┤
+             ┌─┘   └─┐
+         ┌───┤       ├───┐   ┌───┐
+         │ e ├───────┤ c ├───┤ d │
+         ├───┘       └───┘   └───┘
+       ┌─┘
+   ┌───┤
+   │ f │
+   └───┘
+Condition :
+a + b + c == d + c + e == f + e + b
+
+find other numbers based on sum of digits along 1 line(line_sol) :
+1) First we will assign numbers to a, b, c
+2) Then the number is assigned to d
+3) Let us assume that line_sol = a + b + c
+3) Now the value of e = line_sol - c - d
+5) Value of f = line_sol - e - b
+
+For each and every combination of a, b, c we will get different d, e, f.
+We will maintain a list of all these combinations as avail_sol so that we
+can compute the largest value at the end
+
+Same principle for 5-gon
+         ┌───┐
+         │ a │
+         └───┤
+             └────┐
+                  ├───┐         ┌───┐
+                  │ b │         │ d │
+                  ├───┤         ├───┘
+             ┌────┘   └────┐   ┌┘
+         ┌───┤             ├───┤
+         │ i │             │ c │
+         ├───┤             ├───┘
+    ┌────┘   └┐           ┌┘
+┌───┤         ├───┐   ┌───┤    ┌───┐
+│ j │         │ g ├───┤ e ├────┤ f │
+└───┘         └───┤   └───┘    └───┘
+                  └┐
+                   ├───┐
+                   │ h │
+                   └───┘
+
+Condition :
+a + b + c == d + c + e == f + e + g == h + g + i == b + i + j
+
+find other numbers based on sum of digits along 1 line(line_sol) :
+1) Assign numbers to a, b, c
+2) Number is assigned to d
+3) say line_sol = a + b + c
+3) then e = line_sol - c - d
+5) then f = line_sol - e - b
+
+Repeat for all combos of a,b,c
 '''
 
-from itertools import permutations
-from itertools import combinations
-# array of candidate solutions empty at the beginning
-record = []
+# numbers from 1-10
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# choose 5 numbers for inner cells between 1 and 9; there are 9C5 combinations
-# the problem ask for a 16-digit number, so 10 is not to be included in inner cells
-cells = range(1,10)
-inner_cells = [map(int,comb) for comb in combinations(cells,5)]
+"""
+mapping
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+|  |  |  |  |  |  |  |  |   |
+V  V  V  V  V  V  V  V  V   V
+a, b, c, d, e, f, g, h, i,  j
+"""
+def convert_to_num(a, b, c, d, e, f, g, h, i, j):
+    """
+    Function to convert a-j to
+    required string based on the criteria
+    """
+    big_num = {a:0, d:1, f:2, h:3, j:4}
+    break_num = big_num[min(big_num.keys())]
+    nums = [(a,b,c), (d,c,e), (f,e,g), (h,g,i), (j,i,b)]
+    nums = nums[break_num:]+nums[:break_num]
+    string = ''
+    for num_tup in nums:
+        for num in num_tup:
+            string += str(num)
+    return string
 
-# code to calculate min and max couple in an array
-def minCouple(array):
-    answer = array[0]+array[-1]
-    for i in range(len(array)-1):
-        coupleSum = array[i] + array[i+1]
-        if coupleSum < answer:
-            answer = coupleSum
-    return answer
-    
-def maxCouple(array):
-    answer = 0
-    for i in range(len(array)-1):
-        if i==0:
-            coupleSum = array[0]+ array[-1]
-            if coupleSum > answer:
-                answer = coupleSum
-        else:
-            coupleSum = array[i]+ array[i+1]
-            if coupleSum > answer:
-                answer = coupleSum
-    return answer
+avail_sol  = []
 
-# Algorithm
-for array in inner_cells:
-    pivot = array[0]
-    perm_array = array[1:]
-    perms = [map(int,perm) for perm in permutations(perm_array,4)]
-    for perm in perms:
-        checkArray = perm
-        checkArray.insert(0,pivot)
-        outerRing = [el for el in range(1,11) if el not in checkArray]
-        xMax = minCouple(checkArray) + max(outerRing)
-        xMin = maxCouple(checkArray) + min(outerRing)
-        if xMax >= xMin:
-            for x in range(xMin, xMax+1):
-                i = checkArray[0]
-                j = checkArray[1]
-                k = checkArray[2]
-                l = checkArray[3]
-                m = checkArray[4]
+for a in numbers:
+    numbers_b = numbers[:]
+    numbers_b.remove(a)
+    # numbers without a
+    for b in numbers_b:
+        numbers_c = numbers_b[:]
+        numbers_c.remove(b)
+        # numbers without a, b
+        for c in numbers_c:
+            line_sum = a+b+c
+            numbers_d = numbers_c[:]
+            numbers_d.remove(c)
+            # numbers without a, b, c
+            for d in numbers_d:
+                numbers_e = numbers_d[:]
+                numbers_e.remove(d)
+                e = line_sum - c - d
+                # numbers without a, b, c, d
+                if e in numbers_e:
+                    numbers_f = numbers_e[:]
+                    numbers_f.remove(e)
+                    # numbers without a, b, c, d, e
+                    for f in numbers_f:
+                        numbers_g = numbers_f[:]
+                        numbers_g.remove(f)
+                        g = line_sum - e - f
+                        # numbers without a, b, c, d, e, f
+                        if g in numbers_g:
+                            numbers_h = numbers_g[:]
+                            numbers_h.remove(g)
+                            # numbers without a, b, c, d, e, f, g
+                            for h in numbers_h:
+                                numbers_i = numbers_h[:]
+                                numbers_i.remove(h)
+                                i = line_sum - g - h
+                                # check if i is in numbers_i list
+                                if i in numbers_i:
+                                    j = line_sum - i - b
+                                    numbers_j = numbers_i[:]
+                                    numbers_j.remove(i)
+                                    # numbers without a, b, c, d, e, f, g, h, i
+                                    if j in numbers_j:
+                                        ctn = convert_to_num(a, b, c, d, e, f, g, h, i, j)
+                                        avail_sol.append(ctn)
 
-                alpha = x-i-j
-                beta = x-j-k
-                gamma = x-k-l
-                delta = x-l-m
-                theta = x-m-i
-                                
-                outerCalculated = [alpha, beta, gamma, delta, theta]
-                
-                if sorted(outerCalculated) == sorted(outerRing):
-                    a = [alpha, i, j]
-                    b = [beta, j, k]
-                    c = [gamma, k, l]
-                    d = [delta, l, m]
-                    e = [theta, m, i]
-                    min_val = min(alpha, beta, gamma, delta, theta)
-                    if alpha == min_val:
-                        append = a+b+c+d+e
-                    elif beta == min_val:
-                        append = b+c+d+e+a
-                    elif gamma == min_val:
-                        append = c+d+e+a+b 
-                    elif delta == min_val:
-                        append = d+e+a+b+c 
-                    elif theta == min_val:
-                        append = e+a+b+c+d 
-                    l = [str(i) for i in append]
-                    s = ''.join(l)
-                    integer_list = int(s) 
-                    record.append(integer_list)
-print(max(record))
+res = max([int(x) if len(x) == 16 else 0 for x in avail_sol])
+
+print(res)
